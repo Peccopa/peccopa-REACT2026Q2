c9f05c91bbad790209a678eb847c24bc5f2c0651
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-04-24 15:16:20 +0300

    feat: add local storage helpers

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,2 @@
 export const API_URL = 'https://dummyjson.com/products';
+export const LS_KEY = 'search_term';
--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,3 +1,9 @@
-export function saveSearch() {}
+import { LS_KEY } from '@/constants';
 
-export function getSearch() {}
+export function getSearchFromStorage(): string {
+  return localStorage.getItem(LS_KEY) || '';
+}
+
+export function saveSearchToStorage(value: string): void {
+  localStorage.setItem(LS_KEY, value);
+}
