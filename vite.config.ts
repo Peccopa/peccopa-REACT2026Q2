a524c2811dcb74ef3385e8065a4ea57108705aa0
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-05-01 08:53:20 +0300

    chore: add full app path to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/peccopa-REACT2026Q2/class-components/',
   plugins: [react()],
   resolve: {
     alias: {
