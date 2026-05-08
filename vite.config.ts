37409856c136a797350bfca6ab2dc601bc75bd52
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-05-08 10:33:28 +0300

    chore: add unit-testing path to vite.config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/peccopa-REACT2026Q2/class-components/',
+  base: '/peccopa-REACT2026Q2/unit-testing/',
   plugins: [react()],
   resolve: {
     alias: {
