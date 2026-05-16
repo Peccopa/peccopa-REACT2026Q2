6002ea7969932a2c2e5051bf2574c36c2e81fec3
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-05-16 09:08:21 +0300

    refactor: convert Input component to functional component

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -1,24 +1,18 @@
 import type { InputHTMLAttributes } from 'react';
 
-import { Component } from 'react';
-
 import styles from './Input.module.css';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   error?: boolean;
 }
 
-export class Input extends Component<Props> {
-  render() {
-    const { className, error, ...rest } = this.props;
-
-    return (
-      <input
-        className={[styles.input, error ? styles.error : '', className]
-          .filter(Boolean)
-          .join(' ')}
-        {...rest}
-      />
-    );
-  }
+export function Input({ className, error, ...rest }: Props) {
+  return (
+    <input
+      className={[styles.input, error && styles.error, className]
+        .filter(Boolean)
+        .join(' ')}
+      {...rest}
+    />
+  );
 }
