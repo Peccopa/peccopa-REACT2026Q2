be75c512ccf573f2841f2bb1e5ea904cbecb2631
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-05-16 14:25:25 +0300

    refactor: convert Layout component to functional component

--- src/widgets/layout/ui/Layout.tsx
+++ src/widgets/layout/ui/Layout.tsx
@@ -1,23 +1,19 @@
-import { Component } from 'react';
 import { Header } from '@/widgets/header';
 import { Footer } from '@/widgets/footer';
-
-import styles from './Layout.module.css';
 import { Outlet } from 'react-router-dom';
 
+import styles from './Layout.module.css';
+
 interface Props {
-  // children: React.ReactNode;
   className?: string;
 }
 
-export class Layout extends Component<Props> {
-  render() {
-    return (
-      <div className={`${styles.layout} ${this.props.className || ''}`}>
-        <Header />
-        <Outlet />
-        <Footer />
-      </div>
-    );
-  }
+export function Layout({ className }: Props) {
+  return (
+    <div className={`${styles.layout} ${className || ''}`}>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
