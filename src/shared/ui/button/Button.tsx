729bf9d2ab628ecffef2fa65e4b1512951285fc8
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-05-16 09:06:14 +0300

    refactor: convert Button component to functional component

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,21 +1,16 @@
 import type { ButtonHTMLAttributes } from 'react';
 
-import { Component } from 'react';
 import styles from './Button.module.css';
 
 type Props = ButtonHTMLAttributes<HTMLButtonElement>;
 
-export class Button extends Component<Props> {
-  render() {
-    const { children, className, ...rest } = this.props;
-
-    return (
-      <button
-        className={[styles.button, className].filter(Boolean).join(' ')}
-        {...rest}
-      >
-        {children}
-      </button>
-    );
-  }
+export function Button({ children, className, ...rest }: Props) {
+  return (
+    <button
+      className={[styles.button, className].filter(Boolean).join(' ')}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 }
